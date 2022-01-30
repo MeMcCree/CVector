f1bddcb3ee802fda1c8629f93661d82a23e90cc2
MeMcCree <vk_vkopter@mail.ru>
2022-01-31 00:01:36 +0300

    Add methods to vector

--- example.c
+++ example.c
@@ -8,25 +8,31 @@ typedef struct {
 } vec2;
 
 DECLARE_VECTOR(vec2)
+DECLARE_VECTOR(int)
 
 int main() {
 	vec2 v1;
 
-	Vector_vec2 v;
-	VectorInit_vec2(&v);
+	Vec_vec2 v;
+	VecInit_vec2(&v);
+
+	Vec_int vi;
+	VecInit_int(&vi);
 	
 	srand(time(0));
 	for (uint32_t i = 0; i < 10; i++) {
 		v1.x = rand() % 5;
 		v1.y = rand() % 5;
-		VectorPush_vec2(&v, v1);
+		v.Push(&v, v1);
+		vi.Push(&vi, rand() % 99);
 	}
 
 	for (uint32_t i = 0; i < v.size; i++) {
-		vec2 v2 = VectorGet_vec2(&v, i);
-		printf("{%i, %i}\n", v2.x, v2.y);
+		vec2 v2 = v.Get(&v, i);
+		printf("{%i, %i} : %i\n", v2.x, v2.y, vi.Get(&vi, i));
 	}
 
-	VectorFree_vec2(&v);
+	VecFree_vec2(&v);
+	VecFree_int(&vi);
 	return 0;
 }
--- vector.h
+++ vector.h
@@ -8,40 +8,47 @@
 #define CAPACITY_MUL 2
 
 #define DECLARE_VECTOR(T)																															\
-	typedef struct {       																															\
+	typedef struct Vec_##T Vec_##T; 																										\
+	struct Vec_##T {       																															\
 		T* data;					 																																\
 		uint32_t size;			 																															\
 		uint32_t capacity;	 																															\
-	} Vector_##T;					 																															\
+		T (*Get)(Vec_##T*, uint32_t);																											\
+		void (*Set)(Vec_##T*, uint32_t, T);																								\
+		void (*Push)(Vec_##T*, T);																												\
+	};				 																																					\
 \
-	void VectorInit_##T(Vector_##T* v) {																								\
-		v->data = malloc(sizeof(T) * INIT_CAPACITY);																			\
-		v->size = 0;																																			\
-		v->capacity = INIT_CAPACITY;																											\
-	}																																										\
-\
-	void VectorFree_##T(Vector_##T* v) {																								\
+	void VecFree_##T(Vec_##T* v) {																											\
 		free(v->data);																																		\
 	}																																										\
 \
-	T VectorGet_##T(Vector_##T* v, uint32_t idx) {																			\
+	T VecGet_##T(Vec_##T* v, uint32_t idx) {																						\
 		return v->data[idx];																															\
 	}																																										\
 \
-	void VectorSet_##T(Vector_##T* v, uint32_t idx, T val) {														\
+	void VecSet_##T(Vec_##T* v, uint32_t idx, T val) {																	\
 		v->data[idx] = val;																																\
 	}																																										\
 \
-	void VectorExpand_##T(Vector_##T* v) {																							\
+	void VecExpand_##T(Vec_##T* v) {																										\
 		v->capacity *= CAPACITY_MUL;																											\
 		T* tmp = v->data;																																	\
 		v->data = malloc(sizeof(T) * v->capacity);																				\
-		for (uint32_t _idx = 0; _idx < v->size; _idx++) VectorSet_##T(v, _idx, tmp[_idx]);\
+		for (uint32_t _idx = 0; _idx < v->size; _idx++) VecSet_##T(v, _idx, tmp[_idx]);		\
 		free(tmp);																																				\
 	}																																										\
 \
-	void VectorPush_##T(Vector_##T* v, T val) {																					\
-		if (v->size == v->capacity - 1) VectorExpand_##T(v);															\
-		VectorSet_##T(v, v->size++, val);																									\
+	void VecPush_##T(Vec_##T* v, T val) {																								\
+		if (v->size == v->capacity - 1) VecExpand_##T(v);																	\
+		VecSet_##T(v, v->size++, val);																										\
+	} 																																									\
+\
+	void VecInit_##T(Vec_##T* v) {																											\
+		v->data = malloc(sizeof(T) * INIT_CAPACITY);																			\
+		v->size = 0;																																			\
+		v->capacity = INIT_CAPACITY;																											\
+		v->Get = VecGet_##T;																															\
+		v->Set = VecSet_##T;																															\
+		v->Push = VecPush_##T;																														\
 	}
 #endif
